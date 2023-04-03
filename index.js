1a143a1aa591844ea8017b7a1afb4de8f35bccab
codeexpert200 <84447524+codeexpert200@users.noreply.github.com>
2023-04-04 01:30:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ You are receiving this email because you requested a password reset for your acc
 
 Please click on the link below, or paste it in your browser to reset the password of your account.
 
-https://iste501.000webhostapp.com/resetpassword.html?token=${token}
+https://medivance.onrender.com/resetpassword.html?token=${token}
 
 Please note that the link above is valid for one hour from the time you received this email.
 
