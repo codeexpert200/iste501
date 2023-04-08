9b6188b43478aecabe851c7ed190e03fead577f1
codeexpert200 <84447524+codeexpert200@users.noreply.github.com>
2023-04-08 18:37:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -320,27 +320,24 @@ app.post('/resetpassword', async (req, res) => {
   });
 });
 
-app.post('/uploadmedicalrecord', async (req, res) => {
-  try {
-    const userId = req.body.user_id;
-    const file = req.file;
-    const fileName = file.originalname;
-    const filePath = file.path;
-    const fileSize = file.size;
-    const now = new Date();
-    now.setUTCHours(now.getUTCHours() + 4);
-    const timestamp = `${now.getUTCFullYear()}-${String(now.getUTCMonth() + 1).padStart(2, '0')}-${String(now.getUTCDate()).padStart(2, '0')} ${String(now.getUTCHours()).padStart(2, '0')}:${String(now.getUTCMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}`;
-
-    // Save the file path and other details to the database
-    const connection = await pool.getConnection();
-    const [rows, fields] = await connection.execute('INSERT INTO patient_medical_record (patient_id, patient_medical_record_name, patient_medical_record_path, patient_medical_record_size, patient_medical_record_timestamp_create) VALUES (?, ?, ?, ?, ?)', [userId, fileName, filePath, fileSize, timestamp]);
-    connection.release();
-
-    res.status(200).send('File uploaded successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to upload file');
-  }
+app.post('/uploadmedicalrecord', (req, res) => {
+  const userId = req.body.user_id;
+  const file = req.file;
+  const fileName = file.originalname;
+  const filePath = file.path;
+  const fileSize = file.size;
+  const now = new Date();
+  now.setUTCHours(now.getUTCHours() + 4);
+  const timestamp = `${now.getUTCFullYear()}-${String(now.getUTCMonth() + 1).padStart(2, '0')}-${String(now.getUTCDate()).padStart(2, '0')} ${String(now.getUTCHours()).padStart(2, '0')}:${String(now.getUTCMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}`;
+  const query1 = 'INSERT INTO patient_medical_record (patient_id, patient_medical_record_name, patient_medical_record_path, patient_medical_record_size, patient_medical_record_timestamp_create) VALUES (?, ?, ?, ?, ?)';
+  connection.query(query1, [userId, fileName, filePath, fileSize, timestamp], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    } else {
+      res.status(200).send('File uploaded successfully');
+    }
+  });
 });
 
 app.post('/getheartrate', (req, res) => {
