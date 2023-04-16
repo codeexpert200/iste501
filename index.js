a7f1e30c702bba03e9c34356346e83f013ced1b4
Yameen Khan <myk9191@rit.edu>
2023-04-16 07:45:04 +0400

    Updated

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const dbConfig = {
   database: process.env.MYSQL_DATABASE,
 };
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Node.js running on port ${PORT}`);
 });
@@ -43,7 +44,7 @@ app.post('/signin', (req, res) => {
 
   const query1 = 'SELECT user_id, user_type FROM user WHERE user_email = ? AND user_password = SHA2(?, 256)';
   connection.query(query1, [email, password], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -56,7 +57,7 @@ app.post('/signin', (req, res) => {
         if (userType == 'Patient') {
           const query2 = 'SELECT patient_first_name, patient_last_name FROM patient WHERE user_id = ?';
           connection.query(query2, [userId], (error, results) => {
-            
+            connection.end();
             if (error) {
               console.error(error);
               res.status(500).send('Server error');
@@ -78,7 +79,7 @@ app.post('/signin', (req, res) => {
           if (userType == 'Doctor') {
             const query3 = 'SELECT doctor_first_name, doctor_last_name FROM doctor WHERE user_id = ?';
             connection.query(query3, [userId], (error, results) => {
-              
+              connection.end();
               if (error) {
                 console.error(error);
                 res.status(500).send('Server error');
@@ -100,7 +101,7 @@ app.post('/signin', (req, res) => {
           else {
             const query4 = 'SELECT mentor_first_name, mentor_last_name FROM mentor WHERE user_id = ?';
             connection.query(query4, [userId], (error, results) => {
-              
+              connection.end();
               if (error) {
                 console.error(error);
                 res.status(500).send('Server error');
@@ -147,7 +148,7 @@ app.post('/verifyemail', (req, res) => {
     const emergencyContactPhone = req.body.emergencyContactPhone;
     const query1 = 'INSERT INTO user_verify_email (user_verify_email_user_type, user_verify_email_token, user_verify_email_first_name, user_verify_email_last_name, user_verify_email_phone_number, user_verify_email_date_of_birth, user_verify_email_nationality, user_verify_email_gender, user_verify_email_blood_type, user_verify_email_emergency_contact_name, user_verify_email_emergency_contact_phone, user_verify_email_email, user_verify_email_password, user_verify_email_timestamp_expire) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, SHA2(?, 256), ?)';
     connection.query(query1, [userType, token, firstName, lastName, phoneNumber, dateOfBirth, nationality, gender, bloodType, emergencyContactName, emergencyContactPhone, email, password, timestamp], (error, results) => {
-      
+      connection.end();
       if (error) {
         console.error(error);
         res.status(500).send('Server error');
@@ -195,7 +196,7 @@ app.post('/signup', (req, res) => {
   const timestamp = `${now.getUTCFullYear()}-${String(now.getUTCMonth() + 1).padStart(2, '0')}-${String(now.getUTCDate()).padStart(2, '0')} ${String(now.getUTCHours()).padStart(2, '0')}:${String(now.getUTCMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}`;
   const query1 = 'SELECT * FROM user_verify_email WHERE user_verify_email_token = ? AND user_verify_email_timestamp_expire > ?';
   connection.query(query1, [token, timestamp], (error, results1) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -217,7 +218,7 @@ app.post('/signup', (req, res) => {
       const emergencyContactPhone = row.user_verify_email_emergency_contact_phone;
       const query2 = 'INSERT INTO user (user_email, user_password, user_type) VALUES (?, ?, ?)';
       connection.query(query2, [email, password, userType], (error, results2) => {
-        
+        connection.end();
         if (error) {
           console.error(error);
           res.status(500).send('Server error');
@@ -226,14 +227,14 @@ app.post('/signup', (req, res) => {
             const userId = results2.insertId;
             const query3 = 'INSERT INTO patient (user_id, patient_first_name, patient_last_name, patient_phone_number, patient_date_of_birth, patient_nationality, patient_gender, patient_blood_type, patient_emergency_contact_name, patient_emergency_contact_phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
             connection.query(query3, [userId, firstName, lastName, phoneNumber, dateOfBirth, nationality, gender, bloodType, emergencyContactName, emergencyContactPhone], (error, results3) => {
-              
+              connection.end();
               if (error) {
                 console.error(error);
                 res.status(500).send('Server error');
               } else {
                 const query4 = 'DELETE FROM user_verify_email WHERE user_verify_email_email = ?';
                 connection.query(query4, [email], (error, results4) => {
-                  
+                  connection.end();
                   if (error) {
                     console.error(error);
                     res.status(500).send('Server error');
@@ -249,14 +250,14 @@ app.post('/signup', (req, res) => {
               const userId = results2.insertId;
               const query5 = 'INSERT INTO doctor (user_id, doctor_first_name, doctor_last_name, doctor_phone_number, doctor_date_of_birth, doctor_nationality, doctor_gender) VALUES (?, ?, ?, ?, ?, ?, ?)';
               connection.query(query5, [userId, firstName, lastName, phoneNumber, dateOfBirth, nationality, gender], (error, results5) => {
-                
+                connection.end();
                 if (error) {
                   console.error(error);
                   res.status(500).send('Server error');
                 } else {
                   const query6 = 'DELETE FROM user_verify_email WHERE user_verify_email_email = ?';
                   connection.query(query6, [email], (error, results6) => {
-                    
+                    connection.end();
                     if (error) {
                       console.error(error);
                       res.status(500).send('Server error');
@@ -271,15 +272,15 @@ app.post('/signup', (req, res) => {
               const userId = results2.insertId;
               const query7 = 'INSERT INTO mentor (user_id, mentor_first_name, mentor_last_name, mentor_phone_number, mentor_date_of_birth, mentor_nationality, mentor_gender) VALUES (?, ?, ?, ?, ?, ?, ?)';
               connection.query(query7, [userId, firstName, lastName, phoneNumber, dateOfBirth, nationality, gender], (error, results7) => {
-                
+                connection.end();
                 if (error) {
                   console.error(error);
                   res.status(500).send('Server error');
                 } else {
                   const query8 = 'DELETE FROM user_verify_email WHERE user_verify_email_email = ?';
-                  
+                  connection.end();
                   connection.query(query8, [email], (error, results8) => {
-                    
+                    connection.end();
                     if (error) {
                       console.error(error);
                       res.status(500).send('Server error');
@@ -302,7 +303,7 @@ app.post('/checkemail', (req, res) => {
 
   const query1 = 'SELECT user_email FROM user WHERE user_email = ?';
   connection.query(query1, [email], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -327,7 +328,7 @@ app.post('/forgotpassword', (req, res) => {
 // Check if the email exists in the database and update the token and expiration
   const query1 = 'INSERT INTO reset_password (reset_password_token, reset_password_email, reset_password_timestamp_expire) VALUES (?, ?, ?)';
   connection.query(query1, [token, email, timestamp], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -378,7 +379,7 @@ app.post('/resetpassword', async (req, res) => {
   // Find the user with the provided token and check if it's still valid
   const query1 = `SELECT reset_password_email FROM reset_password WHERE reset_password_token = ? AND reset_password_timestamp_expire > ?`;
   connection.query(query1, [token, timestamp], async (err, result) => {
-    
+    connection.end();
     if (err) throw err;
 
     if (result.length === 0) {
@@ -389,7 +390,7 @@ app.post('/resetpassword', async (req, res) => {
       const email = result[0].reset_password_email;
       const query2 = `UPDATE user SET user_password = SHA2(?, 256) WHERE user_email = ?`;
       connection.query(query2, [newPassword, email], (err, results) => {
-        
+        connection.end();
         if (err) throw err;
 
         if (result.length === 0) {
@@ -399,7 +400,7 @@ app.post('/resetpassword', async (req, res) => {
         else {
           const query3 = 'DELETE FROM reset_password WHERE reset_password_email = ?';
           connection.query(query3, [email], (error, results3) => {
-            
+            connection.end();
             if (error) {
               console.error(error);
               res.status(500).send('Server error');
@@ -424,7 +425,7 @@ app.post('/uploadmedicalrecord', upload.fields([{ name: 'pdf_data', maxCount: 1 
   const timestamp = `${now.getUTCFullYear()}-${String(now.getUTCMonth() + 1).padStart(2, '0')}-${String(now.getUTCDate()).padStart(2, '0')} ${String(now.getUTCHours()).padStart(2, '0')}:${String(now.getUTCMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}`;
   const query1 = 'INSERT INTO patient_medical_record (patient_id, patient_medical_record_name, patient_medical_record_size, patient_medical_record_file, patient_medical_record_timestamp_create) VALUES (?, ?, ?, ?, ?)';
   connection.query(query1, [userId, fileName, fileSize, Buffer.from(pdfData, 'base64'), timestamp], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -439,7 +440,7 @@ app.post('/getheartrate', (req, res) => {
 
   const query1 = 'SELECT patient_heart_rate_value, patient_heart_rate_timestamp FROM patient_heart_rate WHERE patient_id = ?';
   connection.query(query1, [userId], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
@@ -461,7 +462,7 @@ app.post('/gettemperature', (req, res) => {
 
   const query1 = 'SELECT patient_temperature_value, patient_temperature_timestamp FROM patient_temperature WHERE patient_id = ?';
   connection.query(query1, [userId], (error, results) => {
-    
+    connection.end();
     if (error) {
       console.error(error);
       res.status(500).send('Server error');
