87d2a84bbcf73069e2c2f11e61471a1a8b725663
Yameen Khan <myk9191@rit.edu>
2023-04-16 08:14:18 +0400

    Updated!

--- index.js
+++ index.js
@@ -26,12 +26,16 @@ const connection = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
-const dbConfig = {
+
+const connection2 = mysql2.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-};
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
@@ -481,10 +485,8 @@ app.post('/gettemperature', (req, res) => {
 
 app.get('/getdoctor', async (req, res) => {
   try {
-    const connection2 = await mysql2.createConnection(dbConfig);
     const [rows] = await connection2.query('SELECT * FROM doctor');
     res.json(rows);
-    connection2.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
@@ -493,10 +495,8 @@ app.get('/getdoctor', async (req, res) => {
 
 app.get('/getmentor', async (req, res) => {
   try {
-    const connection2 = await mysql2.createConnection(dbConfig);
     const [rows] = await connection2.query('SELECT * FROM mentor');
     res.json(rows);
-    connection2.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
