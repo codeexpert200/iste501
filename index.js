a66c72aca2799181a40624782104858037062851
Yameen Khan <myk9191@rit.edu>
2023-05-11 14:58:46 +0400

    Latest

--- index.js
+++ index.js
@@ -972,7 +972,6 @@ app.post('/sendheartsalert', (req, res) => {
       res.status(500).send('Server error');
     } else {
       const emails = results.map(row => row.user_email);
-      console.log(results);
       const msg = {
         from: 'medivance.no.reply@gmail.com',
         to: emails,
